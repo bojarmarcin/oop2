4317ff742dff279d355bb16891575fc1cda6308d
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-14 22:53:15 +0100

    Fix scripts at scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,14 @@ function Button(text) {
 }
 Button.prototype = {
 	create: function() {
+		var self = this;
 		this.$element = $('<button>');
 		this.$element.text(this.text);
-	}
+		this.$element.appendTo($('body'));
+		this.$element.click(function() {
+			alert(self.text);
+		});
 }
-this.$element.click(function() {
-	alert(self.text);
-});
-create: function() {
-	var self = this;
-	this.$element = $('<button>');
-	this.$element.text(this.text);
-	this.$element.click(function() {
-		alert(self.text);
-	});
-}
-this.$element.appendTo($('body'));
 
 var btn1 = new Button('Hello!');
 btn1.create();
